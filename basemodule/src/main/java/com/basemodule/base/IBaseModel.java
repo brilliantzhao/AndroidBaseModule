34d77c68aca1454603a1380879e4bc2dc70a7c36
DESKTOP-V2KIE16\Administrator <137148873@qq.com>
2017-04-12 17:43:24 +0800

    剩余 java.lang.ClassCastException: java.lang.Class cannot be cast to java.lang.reflect.ParameterizedType

--- basemodule/src/main/java/com/basemodule/base/IBaseModel.java
+++ basemodule/src/main/java/com/basemodule/base/IBaseModel.java
@@ -2,17 +2,14 @@ package com.basemodule.base;
 
 import android.content.Context;
 
-import java.lang.reflect.ParameterizedType;
-
 /**
  * des:baseModel
  * Created by xsf
  * on 2016.08.14:50
  */
-public interface IBaseModel extends ParameterizedType {
+public interface IBaseModel {
 
     /**
-     *
      * @param context
      */
     void setTag(Context context);
