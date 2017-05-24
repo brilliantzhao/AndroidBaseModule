f9cb4242d303b99f7fbfeb6e543ca5a3ca459f52
DESKTOP-V2KIE16\Administrator <137148873@qq.com>
2017-05-24 10:51:59 +0800

    添加log日志处理

---
+++ basemodule/src/main/java/com/basemodule/utils/log/Printer.java
@@ -0,0 +1,31 @@
+package com.basemodule.utils.log;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Printer {
+
+    String getFormatLog();
+
+    void d(String message, Object... args);
+
+    void e(String message, Object... args);
+
+    void e(Throwable throwable, String message, Object... args);
+
+    void w(String message, Object... args);
+
+    void i(String message, Object... args);
+
+    void v(String message, Object... args);
+
+    void wtf(String message, Object... args);
+
+    void json(String json);
+
+    void xml(String xml);
+
+    void map(Map map);
+
+    void list(List list);
+}
