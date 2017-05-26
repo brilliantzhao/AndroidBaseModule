56aab05354fe08d39ebb1bb3a136b6abcb89fd45
DESKTOP-V2KIE16\Administrator <137148873@qq.com>
2017-05-26 14:31:47 +0800

    添加fragmetn的懒加载支持

--- basemodule/src/main/java/com/basemodule/base/IBaseFragment.java
+++ basemodule/src/main/java/com/basemodule/base/IBaseFragment.java
@@ -14,9 +14,13 @@ import com.trello.rxlifecycle.LifecycleTransformer;
 import butterknife.ButterKnife;
 
 /**
- * des:基类fragment
- * Created by xsf
- * on 2016.07.12:38
+ * Fragment基类，封装了懒加载的实现
+ * <p>
+ * 1、Viewpager + Fragment情况下，fragment的生命周期因Viewpager的缓存机制而失去了具体意义
+ * 该抽象类自定义新的回调方法，当fragment可见状态改变时会触发的回调方法，和 Fragment 第一次可见时会回调的方法
+ *
+ * @see #onFragmentVisibleChange(boolean)
+ * @see #onFragmentFirstVisible()
  */
 
 /***************使用例子*********************/
@@ -66,9 +70,22 @@ public abstract class IBaseFragment<T extends IBasePresenter, E extends IBaseMod
 
     public RxManager mRxManager;
 
+    //=== 懒加载实现
+    private boolean isFragmentVisible;
+
+    private boolean isReuseView;
+
+    private boolean isFirstVisible;
+
     //##########################   custom variables end  ##########################################
 
     //######################  override methods start ##############################################
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initVariable();
+    }
 
     @Nullable
     @Override
@@ -89,11 +106,63 @@ public abstract class IBaseFragment<T extends IBasePresenter, E extends IBaseMod
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        //如果setUserVisibleHint()在rootView创建前调用时，那么
+        //就等到rootView创建完后才回调onFragmentVisibleChange(true)
+        //保证onFragmentVisibleChange()的回调发生在rootView创建完成之后，以便支持ui操作
+        if (rootView == null) {
+            rootView = view;
+            if (getUserVisibleHint()) {
+                if (isFirstVisible) {
+                    onFragmentFirstVisible();
+                    isFirstVisible = false;
+                }
+                onFragmentVisibleChange(true);
+                isFragmentVisible = true;
+            }
+        }
+        super.onViewCreated(isReuseView ? rootView : view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        initVariable();
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         if (mPresenter != null)
             mPresenter.onDestroy();
         mRxManager.clear();
+    }
+
+    //setUserVisibleHint()在Fragment创建时会先被调用一次，传入isVisibleToUser = false
+    //如果当前Fragment可见，那么setUserVisibleHint()会再次被调用一次，传入isVisibleToUser = true
+    //如果Fragment从可见->不可见，那么setUserVisibleHint()也会被调用，传入isVisibleToUser = false
+    //总结：setUserVisibleHint()除了Fragment的可见状态发生变化时会被回调外，在new Fragment()时也会被回调
+    //如果我们需要在 Fragment 可见与不可见时干点事，用这个的话就会有多余的回调了，那么就需要重新封装一个
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        //setUserVisibleHint()有可能在fragment的生命周期外被调用
+        if (rootView == null) {
+            return;
+        }
+        if (isFirstVisible && isVisibleToUser) {
+            onFragmentFirstVisible();
+            isFirstVisible = false;
+        }
+        if (isVisibleToUser) {
+            onFragmentVisibleChange(true);
+            isFragmentVisible = true;
+            return;
+        }
+        if (isFragmentVisible) {
+            isFragmentVisible = false;
+            onFragmentVisibleChange(false);
+        }
     }
 
     //######################   override methods end  ##############################################
@@ -120,6 +189,58 @@ public abstract class IBaseFragment<T extends IBasePresenter, E extends IBaseMod
     //######################  override custom metohds end  ########################################
 
     //######################      custom metohds start     ########################################
+
+    /**
+     *
+     */
+    private void initVariable() {
+        isFirstVisible = true;
+        isFragmentVisible = false;
+        rootView = null;
+        isReuseView = true;
+    }
+
+    /**
+     * 设置是否使用 view 的复用，默认开启
+     * view 的复用是指，ViewPager 在销毁和重建 Fragment 时会不断调用 onCreateView() -> onDestroyView()
+     * 之间的生命函数，这样可能会出现重复创建 view 的情况，导致界面上显示多个相同的 Fragment
+     * view 的复用其实就是指保存第一次创建的 view，后面再 onCreateView() 时直接返回第一次创建的 view
+     *
+     * @param isReuse
+     */
+    protected void reuseView(boolean isReuse) {
+        isReuseView = isReuse;
+    }
+
+    /**
+     * 去除setUserVisibleHint()多余的回调场景，保证只有当fragment可见状态发生变化时才回调
+     * 回调时机在view创建完后，所以支持ui操作，解决在setUserVisibleHint()里进行ui操作有可能报null异常的问题
+     * <p>
+     * 可在该回调方法里进行一些ui显示与隐藏，比如加载框的显示和隐藏
+     *
+     * @param isVisible true  不可见 -> 可见
+     *                  false 可见  -> 不可见
+     */
+    protected void onFragmentVisibleChange(boolean isVisible) {
+    }
+
+    /**
+     * 在fragment首次可见时回调，可在这里进行加载数据，保证只在第一次打开Fragment时才会加载数据，
+     * 这样就可以防止每次进入都重复加载数据
+     * 该方法会在 onFragmentVisibleChange() 之前调用，所以第一次打开时，可以用一个全局变量表示数据下载状态，
+     * 然后在该方法内将状态设置为下载状态，接着去执行下载的任务
+     * 最后在 onFragmentVisibleChange() 里根据数据下载状态来控制下载进度ui控件的显示与隐藏
+     */
+    protected void onFragmentFirstVisible() {
+
+    }
+
+    /**
+     * @return
+     */
+    protected boolean isFragmentVisible() {
+        return isFragmentVisible;
+    }
 
     //######################    custom metohds end   ##############################################
 }
