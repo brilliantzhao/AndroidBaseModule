c47ebed04aea02ef12baec8888ddec08d5b53670
DESKTOP-V2KIE16\Administrator <137148873@qq.com>
2017-06-02 14:56:55 +0800

    重新整理share数据存储

--- basemodule/src/main/java/com/basemodule/local/sharedpref/SharedPrefUtils.java
+++ basemodule/src/main/java/com/basemodule/local/sharedpref/SharedPrefUtils.java
@@ -1,9 +1,9 @@
 package com.basemodule.local.sharedpref;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import com.basemodule.base.IBaseApplication;
 import com.basemodule.utils.NativeUtil;
 import com.orhanobut.hawk.Hawk;
 
@@ -12,14 +12,29 @@ import com.orhanobut.hawk.Hawk;
  */
 public class SharedPrefUtils {
 
-    private static SharedPreferences sp;
+    private SharedPreferences sp;
+
+    private static SharedPrefUtils instance;
+
+    public static SharedPrefUtils getInstance() {
+        if (instance == null) {
+            synchronized (SharedPrefUtils.class) {
+                if (instance == null) {
+                    instance = new SharedPrefUtils();
+                }
+            }
+        }
+        return instance;
+    }
 
     /**
      * 初始化
+     *
+     * @param context
      */
-    private static void init() {
+    public void init(Context context) {
         if (sp == null) {
-            sp = PreferenceManager.getDefaultSharedPreferences(IBaseApplication.getAppInstance());
+            sp = PreferenceManager.getDefaultSharedPreferences(context);
         }
     }
 
@@ -27,11 +42,9 @@ public class SharedPrefUtils {
      * @param key
      * @param value
      */
-    public static void setSharedIntData(String key, int value) {
-        if (sp == null) {
-            init();
-        }
-        sp.edit().putInt(key, value).commit();
+    public void setSharedIntData(String key, int value) {
+        if (sp != null)
+            sp.edit().putInt(key, value).commit();
     }
 
     /**
@@ -39,22 +52,20 @@ public class SharedPrefUtils {
      * @param defValue
      * @return
      */
-    public static int getSharedIntData(String key, int defValue) {
-        if (sp == null) {
-            init();
-        }
-        return sp.getInt(key, defValue);
+    public int getSharedIntData(String key, int defValue) {
+        if (sp != null)
+            return sp.getInt(key, defValue);
+        else
+            return defValue;
     }
 
     /**
      * @param key
      * @param value
      */
-    public static void setSharedlongData(String key, long value) {
-        if (sp == null) {
-            init();
-        }
-        sp.edit().putLong(key, value).commit();
+    public void setSharedlongData(String key, long value) {
+        if (sp != null)
+            sp.edit().putLong(key, value).commit();
     }
 
     /**
@@ -62,22 +73,20 @@ public class SharedPrefUtils {
      * @param defValue
      * @return
      */
-    public static long getSharedlongData(String key, long defValue) {
-        if (sp == null) {
-            init();
-        }
-        return sp.getLong(key, defValue);
+    public long getSharedlongData(String key, long defValue) {
+        if (sp != null)
+            return sp.getLong(key, defValue);
+        else
+            return defValue;
     }
 
     /**
      * @param key
      * @param value
      */
-    public static void setSharedFloatData(String key, float value) {
-        if (sp == null) {
-            init();
-        }
-        sp.edit().putFloat(key, value).commit();
+    public void setSharedFloatData(String key, float value) {
+        if (sp != null)
+            sp.edit().putFloat(key, value).commit();
     }
 
     /**
@@ -85,22 +94,20 @@ public class SharedPrefUtils {
      * @param defValue
      * @return
      */
-    public static Float getSharedFloatData(String key, Float defValue) {
-        if (sp == null) {
-            init();
-        }
-        return sp.getFloat(key, defValue);
+    public Float getSharedFloatData(String key, Float defValue) {
+        if (sp != null)
+            return sp.getFloat(key, defValue);
+        else
+            return defValue;
     }
 
     /**
      * @param key
      * @param value
      */
-    public static void setSharedBooleanData(String key, boolean value) {
-        if (sp == null) {
-            init();
-        }
-        sp.edit().putBoolean(key, value).commit();
+    public void setSharedBooleanData(String key, boolean value) {
+        if (sp != null)
+            sp.edit().putBoolean(key, value).commit();
     }
 
     /**
@@ -108,22 +115,20 @@ public class SharedPrefUtils {
      * @param defValue
      * @return
      */
-    public static Boolean getSharedBooleanData(String key, boolean defValue) {
-        if (sp == null) {
-            init();
-        }
-        return sp.getBoolean(key, defValue);
+    public Boolean getSharedBooleanData(String key, boolean defValue) {
+        if (sp != null)
+            return sp.getBoolean(key, defValue);
+        else
+            return defValue;
     }
 
     /**
      * @param key
      * @param value
      */
-    public static void setSharedStringData(String key, String value) {
-        if (sp == null) {
-            init();
-        }
-        sp.edit().putString(key, value).commit();
+    public void setSharedStringData(String key, String value) {
+        if (sp != null)
+            sp.edit().putString(key, value).commit();
     }
 
     /**
@@ -131,11 +136,27 @@ public class SharedPrefUtils {
      * @param defValue
      * @return
      */
-    public static String getSharedStringData(String key, String defValue) {
-        if (sp == null) {
-            init();
-        }
-        return sp.getString(key, defValue);
+    public String getSharedStringData(String key, String defValue) {
+        if (sp != null)
+            return sp.getString(key, defValue);
+        else
+            return defValue;
+    }
+
+    /**
+     * 删除某一项的值
+     *
+     * @param key
+     */
+    public void sharedPrefRemove(String key) {
+        sp.edit().remove(key).commit();
+    }
+
+    /**
+     * 清空所有的sharedPref的值
+     */
+    public void sharedPrefClear() {
+        sp.edit().clear().commit();
     }
 
     /**
@@ -144,17 +165,19 @@ public class SharedPrefUtils {
      * @param key
      * @param value
      */
-    public static void setSharedObject(String key, Object value) {
+    public void setHawkObject(String key, Object value) {
         Hawk.put(key, value);
     }
 
     /**
+     * 获取Hawk数据
+     *
      * @param key
      * @param defaultValue
      * @param <T>
      * @return
      */
-    public static <T> T getSharedObject(String key, T defaultValue) {
+    public <T> T getHawkObject(String key, T defaultValue) {
         if (!NativeUtil.isEmpty(key) && Hawk.contains(key)) {
             return Hawk.get(key, defaultValue);
         } else {
@@ -163,33 +186,39 @@ public class SharedPrefUtils {
     }
 
     /**
+     * 判断是否包含某一条Hawk数据
+     *
+     * @param key
+     * @return
+     */
+    public boolean containsHawkObject(String key) {
+        return Hawk.contains(key);
+    }
+
+    /**
+     * 获取Hawk存储条数
+     *
+     * @return
+     */
+    public long getHawkCount() {
+        return Hawk.count();
+    }
+
+    /**
+     * 删除某一条Hawk数据
+     *
      * @param key
      */
-    public static void deleteSharedObject(String key) {
+    public void deleteHawkObject(String key) {
         if (!NativeUtil.isEmpty(key) && Hawk.contains(key)) {
             Hawk.delete(key);
         }
     }
 
     /**
-     * @param key
-     * @return
+     * 清空Hawk所有数据
      */
-    public static boolean containsSharedObject(String key) {
-        return Hawk.contains(key);
-    }
-
-    /**
-     * @return
-     */
-    public static long getSharedObjectCount() {
-        return Hawk.count();
-    }
-
-    /**
-     *
-     */
-    public static void clearSharedObject() {
+    public void clearHawkObject() {
         Hawk.deleteAll();
     }
 }
