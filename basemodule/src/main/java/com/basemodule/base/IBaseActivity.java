08380ab3ed08f2231aa76568a644b47a9ead8765
DESKTOP-V2KIE16\Administrator <137148873@qq.com>
2017-06-15 12:17:07 +0800

    baseActivity中添加注释

--- basemodule/src/main/java/com/basemodule/base/IBaseActivity.java
+++ basemodule/src/main/java/com/basemodule/base/IBaseActivity.java
@@ -51,15 +51,6 @@ import butterknife.ButterKnife;
 //    public void initView() {
 //    }
 //}
-
-/**
- * BaseActivity带有两个泛型,分别限制是继承BasePresenter与BaseModel,通过TUtil生成两个Model与Presenter中,同时调用Presenter的setVM方传入View与Model。
- * <p>
- * 此时我们边能够再Activity中通过mPresenter调用Presenter,在Presenter中通过mView与mModel分别调用View与Model了。
- *
- * @param <T>
- * @param <E>
- */
 public abstract class IBaseActivity<T extends IBasePresenter, E extends IBaseModel> extends AppCompatActivity
         implements IBaseView {
 
